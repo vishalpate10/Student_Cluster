c006010271dc57592cebd80c554da963503ad282
Vishal pate <vprakashpate@gmail.com>
2025-07-28 10:47:55 +0530

    app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ st.markdown("""
     .footer {
         position: fixed;
         bottom: 0;
-        left: 1;
+        left: 0;
         color: #888;
         font-size: 20px;
         padding: 12px 20px;
